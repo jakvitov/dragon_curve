cf3abe4bae283a55b3c19fea1d9b1c18b3ef6be9
Jakub Vítovec <jakvitov@gmail.com>
2023-09-03 21:42:35 +0200

    Mouse move tracker implemented

--- build/dragon_curve.js
+++ build/dragon_curve.js
@@ -1,14 +1,56 @@
-var prepareCanvas = function () {
-    var canvas = document.getElementById("drawBoard");
-    var ctx = canvas.getContext("2d");
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+var drawing = false;
+var drawingStart = { x: null, y: null };
+var drawLine = function (from, to, ctx) {
     ctx.beginPath();
-    ctx.moveTo(300, 300);
-    ctx.lineTo(400, 400);
+    ctx.moveTo(from.x, from.y);
+    ctx.lineTo(to.x, to.y);
     ctx.stroke();
-    return ctx;
+};
+//Clear the canvas
+var clearCanvas = function (context) {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+};
+//Given a line from a |---| b, split the line in half and create both hypotenuses of it by creating point C
+var splitLine = function (a, b, context) {
+    //Middle of the line
+    var middle = { x: (a.x + b.x) / 2, y: (a.y + b.y) / 2 };
+    //Relative coordinates of A in coord system, where middle point is the beginning of a cartesian coord system.
+    var relativeA = ({ x: a.x - middle.x, y: a.y - middle.y });
+    //Rotate A around S by Pi degrees -> relativeC
+    //We use standard rotation matrices with precalculated sines and cosines of the used angles (Pi/2)
+    var relativeC = { x: -relativeA.y, y: relativeA.x };
+    //Transform the relativeC to normal coordinates
+    var c = { x: relativeC.x + middle.x, y: relativeC.y + middle.y };
+    console.log(c);
+    drawLine(c, a, context);
+    drawLine(c, b, context);
 };
 var start = function () {
-    prepareCanvas();
+    //splitLine({x: 300, y : 300}, {x: 400, y : 400}, context)
 };
-document.getElementById("drawButton").addEventListener("click", start);
+var canvas = document.getElementById("drawBoard");
+var context = canvas.getContext("2d");
+//Register client first click and start drawing line from there
+canvas.addEventListener("click", function (ev) {
+    if (drawing) {
+        var x = ev.clientX - canvas.getBoundingClientRect().left;
+        var y = ev.clientY - canvas.getBoundingClientRect().top;
+        clearCanvas(context);
+        drawLine(drawingStart, { x: x, y: y }, context);
+        drawing = false;
+        return;
+    }
+    //Get coordinates of client click and set it as a drawing start
+    drawingStart.x = ev.clientX - canvas.getBoundingClientRect().left;
+    drawingStart.y = ev.clientY - canvas.getBoundingClientRect().top;
+    drawing = true;
+});
+canvas.addEventListener("mousemove", function (ev) {
+    if (!drawing) {
+        return;
+    }
+    var x = ev.clientX - canvas.getBoundingClientRect().left;
+    var y = ev.clientY - canvas.getBoundingClientRect().top;
+    clearCanvas(context);
+    drawLine(drawingStart, { x: x, y: y }, context);
+});
--- dragon_curve.ts
+++ dragon_curve.ts
@@ -1,20 +1,77 @@
+let drawing : boolean = false;
+let drawingStart : Coord = {x : null, y : null};
 
-
-const prepareCanvas = () => {
-    const canvas : any= document.getElementById("drawBoard");
-    const ctx : any= canvas.getContext("2d");
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
+interface Coord {
+    x : number;
+    y : number;
+}
+const drawLine = (from : Coord, to : Coord, ctx : any) : void => {
     ctx.beginPath();
-    ctx.moveTo(300, 300);
-    ctx.lineTo(400, 400);
+    ctx.moveTo(from.x, from.y);
+    ctx.lineTo(to.x, to.y);
     ctx.stroke();
-
-    return ctx;
 }
 
-const start = () => {
-    prepareCanvas();
+//Clear the canvas
+const clearCanvas = (context : any) => {
+    context.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-document.getElementById("drawButton").addEventListener("click", start);
+//Given a line from a |---| b, split the line in half and create both hypotenuses of it by creating point C
+const splitLine = (a : Coord, b : Coord, context : any) : void => {
+    //Middle of the line
+    const middle : Coord = {x : (a.x + b.x)/2, y : (a.y + b.y)/2};
+
+    //Relative coordinates of A in coord system, where middle point is the beginning of a cartesian coord system.
+    const relativeA : Coord = ({x : a.x - middle.x, y : a.y - middle.y});
+
+    //Rotate A around S by Pi degrees -> relativeC
+    //We use standard rotation matrices with precalculated sines and cosines of the used angles (Pi/2)
+    const relativeC : Coord = {x : -relativeA.y, y :  relativeA.x};
+
+    //Transform the relativeC to normal coordinates
+    const c : Coord = {x : relativeC.x + middle.x, y : relativeC.y + middle.y};
+
+    console.log(c);
+
+    drawLine(c, a, context);
+    drawLine(c, b, context);
+
+}
+
+const start = () : void => {
+
+    //splitLine({x: 300, y : 300}, {x: 400, y : 400}, context)
+
+}
+
+let canvas : any = document.getElementById("drawBoard");
+const context : any= canvas.getContext("2d");
+
+//Register client first click and start drawing line from there
+canvas.addEventListener("click", (ev) => {
+
+    if (drawing) {
+        let x : number = ev.clientX - canvas.getBoundingClientRect().left;
+        let y : number = ev.clientY - canvas.getBoundingClientRect().top;
+        clearCanvas(context);
+        drawLine(drawingStart, {x : x, y : y}, context);
+        drawing = false;
+        return;
+    }
+
+    //Get coordinates of client click and set it as a drawing start
+    drawingStart.x = ev.clientX - canvas.getBoundingClientRect().left;
+    drawingStart.y = ev.clientY - canvas.getBoundingClientRect().top;
+    drawing = true;
+})
+
+canvas.addEventListener("mousemove", (ev) => {
+    if (!drawing){
+        return;
+    }
+    let x : number = ev.clientX - canvas.getBoundingClientRect().left;
+    let y : number = ev.clientY - canvas.getBoundingClientRect().top;
+    clearCanvas(context);
+    drawLine(drawingStart, {x : x, y : y}, context);
+})
